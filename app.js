e782f509fad34210e9a062c71dee7b58c7adb80e
oliviakim217 <oliviakim217@users.noreply.github.com>
2022-04-11 15:30:52 -0400

    Add mongoose-encryption plugin

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/userDB");
@@ -17,6 +19,11 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
+
+
+// Encryption - Add the encrypt package as a plugin
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] }); 
 
 const User = mongoose.model("User", userSchema);
 
@@ -64,7 +71,7 @@ app.post("/login", function(req, res) {
             } 
         }
     });
-})
+});
 
 
 
@@ -75,7 +82,7 @@ app.post("/login", function(req, res) {
 
 
 app.listen(3000, function() {
-    console.log("Server started on port 3000.")
+    console.log("Server started on port 3000.");
 });
 
 
