11ca3aadab10923f6022a3590798a08f9a2bf349
oliviakim217 <oliviakim217@users.noreply.github.com>
2022-04-11 14:47:21 -0400

    Level 1 - Register Users with Username and Password

--- app.js
+++ app.js
@@ -1,12 +1,71 @@
+//#region
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+//#endregion
+
+
+
+// Routes
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    const newUser = new User ({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err) {
+        if (!err) {
+            res.render("secrets");
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post("/login", function(req, res) {
+    User.findOne({email: req.body.username}, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(foundUser);
+            if (foundUser) { // check to see if there was a found user.
+                if (foundUser.password === req.body.password) {
+                    res.render("secrets");
+                }
+            } 
+        }
+    });
+})
+
 
 
 
